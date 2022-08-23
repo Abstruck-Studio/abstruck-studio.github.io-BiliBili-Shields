40aa37a0502c059a56fc7bb0059403b4c6900a4a
够立小激 <84974878+Goulixiaoji@users.noreply.github.com>
2022-08-23 16:24:45 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -5,5 +5,7 @@ function getQueryString(name) {
 }
 
 function swichFansUrl(mid) {
-
+  const url_str1 = "https://img.shields.io/badge/dynamic/json?color=blue&logo=bilibili&style=social&label=Follow&query=data.follower&url=https%3A%2F%2Fapi.bilibili.com%2Fx%2Frelation%2Fstat%3Fvmid%3D";
+  const url_str2 = "%26jsonp%3Djsonp";
+  return url_str1 + mid + url_str2;
 }
